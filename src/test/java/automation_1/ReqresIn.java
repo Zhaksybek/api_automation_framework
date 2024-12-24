8d71684e919ccdd4f2b087bf66393c07ea76c4c1
zacksatylgan <166334246+zacksatylgan@users.noreply.github.com>
2024-12-24 11:12:15 -0600

    json path

--- src/test/java/automation_1/ReqresIn.java
+++ src/test/java/automation_1/ReqresIn.java
@@ -17,6 +17,33 @@ public class ReqresIn {
         // prettyPrint to able to get pretty response
         response.prettyPrint();
 
+        // Step - 1 we have to print json object
+        System.out.println("======Print json object========================");
+        response.prettyPrint();
+
+        // Step - 2 we have to find json path of our target
+
+        // Step - 3 we have to use response.jsonPath().getString("data.first_name")
+
+        String firstName = response.jsonPath().getString("data.first_name");
+
+        System.out.println(  "Expected first name is: "+firstName  );
+
+        String lastName = response.jsonPath().getString("data.last_name");
+
+        System.out.println("Expected last name is: " + lastName);
+
+       // ==================================
+        // data.email
+
+        String email = response.jsonPath().getString("data.email");
+        System.out.println("Expected email is: "+ email);
+
+
+
+
+        // Validate Response body, use Json path. Validate first name is Janet
+        //$.data.first_name
 
 
     }
