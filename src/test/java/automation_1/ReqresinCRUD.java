d74e2d79f1a1726214568c78c97a2901516e3d19
zacksatylgan <166334246+zacksatylgan@users.noreply.github.com>
2024-12-24 12:08:48 -0600

    Mu comment

--- src/test/java/automation_1/ReqresinCRUD.java
+++ src/test/java/automation_1/ReqresinCRUD.java
@@ -1,10 +1,18 @@
 package automation_1;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ReqresinCRUD {
 
     @Test
@@ -21,10 +29,89 @@ public class ReqresinCRUD {
 
         Assert.assertEquals(200, response.statusCode() );
 
+    }
 
+
+    @Test
+    public void test_2_createUser(){
+        String url = "https://reqres.in/api/users";
+
+        // Style for request body => we can use string
+        String requestBody = "{\n" +
+                "    \"name\": \"Jason Born\",\n" +
+                "    \"job\": \"QA Software Tester\"\n" +
+                "}";
+
+        Response response = RestAssured.given().contentType(ContentType.JSON)
+                .body( requestBody )
+                .post( url );
+
+        Assert.assertEquals(201, response.statusCode() );
+
+        System.out.println("=======Test Passed!===========");
+
+    }
+
+    @Test
+    public void test_3_createUserMap(){
+        String url = "https://reqres.in/api/users";
+
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("name", "Mary Katy");
+        requestBody.put("job", "Product Manager");
+
+        Response response = RestAssured.given().contentType(ContentType.JSON)
+                .body( requestBody )
+                .post( url );
+
+        Assert.assertEquals(201, response.statusCode() );
+
+        System.out.println("=======Test Passed!===========");
+
+    }
+
+    @Test
+    public void test_4_updateUser(){
+
+        String url ="https://reqres.in/api/users/2";
+
+        Map<String ,Object> requestBody = new HashMap<>();
+        requestBody.put("name", "Jennifer");
+        requestBody.put("job", "Scrum Master");
+
+        Response response = RestAssured.given()
+                        .contentType(ContentType.JSON)
+                        .body(requestBody)
+                        .put( url );
+
+        Assert.assertEquals(200, response.statusCode() );
+
+        String name = response.jsonPath().getString("name");
+        String job = response.jsonPath().getString("job");
+
+        System.out.println("Name is : "+ name);
+        System.out.println("Job is: "+ job);
+
+        Assert.assertEquals("Jennifer", name );
+        Assert.assertEquals("Scrum Master", job );
+
+    }
+
+
+    @Test
+    public void test_5_deleteUser(){
+        String url = "https://reqres.in/api/users/2";
+
+        Response response = RestAssured.delete(url);
+
+        Assert.assertEquals(204, response.statusCode()  );
 
 
     }
+
+
+
+
 
 
 
